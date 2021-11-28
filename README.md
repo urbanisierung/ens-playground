1b07b183b2cc8c8c7075f11136b2f9f1178687fc
Adam <i@adamurban.de>
2021-11-28 07:24:57 +0100

    feat(playground): get text

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # ENS Playground
 
+Good reads:
+
+- [https://docs.ens.domains/dapp-developer-guide/resolving-names](https://docs.ens.domains/dapp-developer-guide/resolving-names)
+
+Libraries:
+
+- [https://www.npmjs.com/package/web3](https://www.npmjs.com/package/web3)
+- [https://www.npmjs.com/package/@ensdomains/ensjs](https://www.npmjs.com/package/@ensdomains/ensjs)
+
 ```bash
 npm run start
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import ENS, { getEnsAddress } from '@ensdomains/ensjs'
 import Web3 from 'web3'
 
+let ens
+
 async function main(name: string) {
   const infura = process.env.INFURA
   const provider = new Web3.providers.HttpProvider(infura)
-  const ens = new ENS({ provider, ensAddress: getEnsAddress('1') })
+  ens = new ENS({ provider, ensAddress: getEnsAddress('1') })
 
   const address = await ens.name(name).getAddress() // 0x123
   console.log(`address of ${name} is ${address}`)
+
+  await printText(name, `url`)
+  await printText(name, `email`)
+}
+
+async function printText(name: string, type: string) {
+  const data = await ens.name(name).getText(type)
+  console.log(`${type}: ${data}`)
 }
 
 // main(`resolver.eth`)
